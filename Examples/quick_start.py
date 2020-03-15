28e0eae06eec4775163ba5c160d5f63a5b2ab31f
Sindre <sindre.n.olufsen@ntnu.no>
2020-03-15 17:51:08 +0100

    Added new example data and updated the quick start file

--- Examples/quick_start.py
+++ Examples/quick_start.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(format='%(name)s:%(levelname)s:%(message)s', level=logging.INFO)
 
 # Path to folder containing images
-path = r'./challenge2/' # Use this formatting on Linux and Mac OS
+path = r'./example_data/' # Use this formatting on Linux and Mac OS
 #path = r'c:\path\to\example_data\\'  # Use this formatting on Windows
 
 # Generate image instance containing all images found in the folder
@@ -14,19 +14,23 @@ images = dic.IO.image_stack_from_folder(path, file_type='.tif')
 
 
 # Generate mesh
-mesher = dic.Mesher(deg_e=3, deg_n=3,type="Q4")
-mesh = mesher.mesh(images,Xc1=20,Xc2=1980,Yc1=19,Yc2=480,n_ely=25,n_elx=80, GUI=False)
+mesher = dic.Mesher(deg_e=3, deg_n=3,type="q4")
 
+# If you want to see use a GUI, set GUI=True below
+mesh = mesher.mesh(images,Xc1=316,Xc2=523,Yc1=209,Yc2=1055,n_ely=36,n_elx=9, GUI=False)
 
 # Instantiate settings object and set some settings manually
 settings = dic.DICInput(mesh, images)
-settings.max_nr_im = 40
+settings.max_nr_im = 500
 settings.ref_update = [15]
 settings.maxit = 20
 settings.tol = 1.e-6
 settings.interpolation_order = 4
 # If you want to access the residual fields after the analysis, this should be set to True
-settings.store_internals = False
+settings.store_internals = True
+
+# This setting defines the behaviour when convergence is not obtained
+settings.noconvergence = "ignore"
 
 # Instantiate job object
 job = dic.DICAnalysis(settings)
@@ -35,12 +39,11 @@ job = dic.DICAnalysis(settings)
 dic_results = job.run()
 
 # Calculate field values
-fields = dic.post.viz.Fields(dic_results,seed=201)
+fields = dic.post.viz.Fields(dic_results,upscale=10)
 
 # Show a field
 viz = dic.Visualizer(fields,images=images)
 
-import  matplotlib.pyplot as plt
 # Uncomment the line below to see the results
-viz.show(field="displacement", component = (1,1), frame = 1,cmap=plt.cm.jet,vmin=-0.5,vmax=0.5)
+# viz.show(field="displacement", component = (1,1), frame=-1)
 
