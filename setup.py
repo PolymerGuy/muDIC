c2c54018ed8c9aba2eaf3a82bc79c2cb56878d01
Sindre Nordmark Olufsen <sindre.n.olufsen@ntnu.no>
2021-04-15 13:37:56 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="muDIC",
-    version="0.2.0",
+    version="0.2.1",
     author="PolymerGuy",
     author_email="sindre.n.olufsen@ntnu.no",
     description="A digital image correlation toolkit",
